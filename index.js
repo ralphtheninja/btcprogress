1bb0d9757101347ce40a6aad9f51750c5771bc0a
Lars-Magnus Skog <lars.magnus.skog@gmail.com>
2013-06-09 04:45:10 +0200

    use blockchain.info to get balance for address

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var request = require('request')
+
 var Canvas = require('canvas')
 var canvas = new Canvas(47, 267)
 var ctx = canvas.getContext('2d')
@@ -48,14 +50,41 @@ function loadImage(src, cb) {
   img.src = src
 }
 
+function percentage(address, limit, cb) {
+  var url = 'https://blockchain.info/address/' + address + '?format=json'
+  // TODO stream result from request and use json parse stream instead
+  request(url, function (err, response, body) {
+    if (err) return cb(err)
+    if (response.statusCode != 200) return cb(new Error('Invalid response'))
+    try {
+      var obj = JSON.parse(body)
+      if (obj.final_balance) {
+        cb(null, 100 * obj.final_balance / (limit * 100000000))
+      } else {
+        cb(new Error('Missing final_balance property'))
+      }
+    } catch (error) {
+      cb(new Error('Failed to parse JSON'))
+    }
+  })
+}
+
 if (!module.parent && !process.browser) {
   require('http').createServer(function (req, res) {
-    var percent = Math.floor(Math.random() * 100)
-    draw(percent, ctx, function (err) {
-      if (err) return console.log(err)
-      res.writeHead(200, { 'Content-Type': 'text/html' })
-      res.write('<img src="' + canvas.toDataURL() + '" />')
-      res.end('<br>' + percent + '%')
+    // using this as a test case
+    // https://blockchain.info/address/1EFMGCH6ngtZcXpY75vz8Sq8Q7TZUrp7jR
+    var address = '1EFMGCH6ngtZcXpY75vz8Sq8Q7TZUrp7jR'
+    var limit = 15
+    percentage(address, limit, function (err, result) {
+      // print result with one decimal accuracy
+      if (err) return
+      var percent = Math.floor(10 * result)/10
+      draw(percent, ctx, function (err) {
+        if (err) return console.log(err)
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write('<img src="' + canvas.toDataURL() + '" />')
+        res.end('<br>' + percent + '%')
+      })
     })
   }).listen(3000)
   console.log('Server started on port 3000')
