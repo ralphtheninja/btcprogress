3bf26ad684521ed1b3342972d12d4ad1fbeef395
Lars-Magnus Skog <ralphtheninja@riseup.net>
2017-08-21 13:34:57 +0200

    fix standard

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var path = require('path')
 var request = require('request')
 var paramify = require('paramify')
 var opts = require('optimist').argv
@@ -5,20 +6,18 @@ var stack = require('stack')
 var ecstatic = require('ecstatic')
 var bar = require('canvas-progress-bar')()
 
-//20 second microcache, to stay under the blockchain
-//api rate limit.
+// 20 second microcache, to stay under the blockchain api rate limit.
 // https://blockchain.info/q
 
 var cache = {}
 
-function balance(address, cb) {
-  if (cache[address])
-    return cb(null, cache[address])
+function balance (address, cb) {
+  if (cache[address]) { return cb(null, cache[address]) }
 
   var options = {
-          uri: 'https://blockchain.info/address/' + address + '?format=json'
-        , json: true
-      }
+    uri: 'https://blockchain.info/address/' + address + '?format=json',
+    json: true
+  }
 
   request.get(options, function (err, _, body) {
     if (err) return cb(err)
@@ -36,7 +35,7 @@ function middleware () {
   return function (req, res, next) {
     req.resume()
 
-    function error(err) {
+    function error (err) {
       if (next) return next(err)
       res.writeHead(400, { 'Content-Type': 'text/html' })
       res.end(err.message || err)
@@ -46,11 +45,12 @@ function middleware () {
 
     if (!match(':address/:balance')) {
         // TODO return root index.html
-        if (next) return next()
-        error('Missing address and/or balance')
+      if (next) return next()
+      error('Missing address and/or balance')
     }
 
     balance(match.params.address, function (err, data) {
+      if (err) return error(err)
       var percent = data / 100000000 / (match.params.balance || 1)
       bar.progress(percent)
       bar.pngStream().pipe(res)
@@ -66,7 +66,7 @@ var port = opts.port || 8080
 
 if (!module.parent && !process.browser) {
   require('http').createServer(
-    stack(middleware(),ecstatic(__dirname+'/static'))
+    stack(middleware(), ecstatic(path.join(__dirname, 'static')))
   ).listen(port, function () {
     console.log('Server started on port ' + port)
   })
